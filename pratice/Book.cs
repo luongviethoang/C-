46cc0d3182a68ef402fa697a2659b8460c1ca4bf
hoàng0809 <91303390+luongviethoang@users.noreply.github.com>
2022-07-14 11:12:32 +0700

    done

---
+++ pratice/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTest
+{
+    class Book : Product
+    {
+        public Book(int iD, string name, double price) : base(iD, name, price)
+        {
+        }
+
+        public override double computeTax()
+        {
+            return this.ProductPrice * 5 / 100;
+        }
---
+++ pratice/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTest
+{
+    class Phone : Product
+    {
+        public Phone(int iD, string name, double price) : base(iD, name, price)
+        {
+        }
+        public override double computeTax()
+        {
+            return this.ProductPrice * 10 / 100;
+        }
+    }
+}
